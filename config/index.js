c6ab90d2f620ea8ea358a49f0a22bb169b3d9d7e
douhaolzx <59857342+douhaolzx@users.noreply.github.com>
2022-09-21 19:51:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "okW8j59J2CTz_r6hBrFXtJFeHB7o",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ECivjyGoU5Za8XNFitmrLjhkLGglAlARyhRCEZAZ2Lk",
+      useTemplateId: "UNKNKcIGZAhO6xNkbia7YE4LhgjnOch6Jqs5Gpg97Jg",
       // 所在省份
       province: "上海",
       // 所在城市
@@ -79,7 +79,6 @@ export const config = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {"type": "*生日", "name": "宝贝", "year": "1995", "date": "09-29"},
-        {"type": "节日", "name": "订婚纪念日", "year": "2022", "date": "10-06"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
@@ -87,7 +86,7 @@ export const config = {
         // 在一起的日子
         {"keyword": "love_day", date: "2018-04-02"},
         // 订婚纪念日
-        {"keyword": "marry_day", date: "2022-10-09"},
+        {"keyword": "marry_day", date: "2022-10-06"},
       ]
     },
     {
